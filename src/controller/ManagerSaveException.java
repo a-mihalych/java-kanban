a6392b434889f76f8878fa861f4e8183ac95e68b
a-mihalych <a-mihalych@yandex.ru>
2022-09-13 12:02:15 +0400

    feat: добавить: исключение ManagerSaveException

---
+++ src/controller/ManagerSaveException.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class ManagerSaveException extends RuntimeException {
+
+    public ManagerSaveException() {}
+
+    public ManagerSaveException(String message) {
+        super(message);
+    }
+}

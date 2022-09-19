ee2c8ca17ba753b549c28b01455ce79e0ecddbf6
a-mihalych <a-mihalych@yandex.ru>
2022-09-19 11:24:28 +0400

    fix: добавить FileBackedTasksManager implements TaskManager

--- src/controller/FileBackedTasksManager.java
+++ src/controller/FileBackedTasksManager.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class FileBackedTasksManager extends InMemoryTaskManager {
+public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
 
     private File file;
 

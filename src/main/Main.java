ca39bf10fc2adea16461defff7d31643496a8853
a-mihalych <a-mihalych@yandex.ru>
2022-10-18 20:40:17 +0400

    feat: добавить класс KVTaskClient

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 import controller.HttpTaskServer;
 import controller.KVServer;
+import controller.KVTaskClient;
 
 import java.io.IOException;
 
@@ -8,5 +9,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        new HttpTaskServer().start();
         new KVServer().start();
+        KVTaskClient client = new KVTaskClient("http://localhost:8078");
+        client.put("тест1", "один");
+        client.put("тест2", "два");
+        client.put("тест3", "три");
+        System.out.println(client.load("тест2"));
+        client.put("тест4", "четыре");
+        System.out.println(client.load("тест3"));
+        client.put("тест2", "Д-В-А");
+        System.out.println(client.load("тест2"));
     }
 }
